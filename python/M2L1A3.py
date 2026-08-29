69e6d0db6f4ca0eacf78c691628b93cf0879adde
Biswareet Panda <biswa.gunu2003@gmail.com>
2026-08-29 16:19:10 +0530

    M2

---
+++ python/M2L1A3.py
@@ -0,0 +1,14 @@
+print("1. Bike")
+print("2. Car")
+print("3. Auto")
+
+choice = int(input("Choose your ride: "))
+
+if choice == 1:
+    print("Bike selected")
+elif choice == 2:
+    print("Car selected")
+elif choice == 3:
+    print("Auto selected")
+else:
+    print("Invalid choice")
